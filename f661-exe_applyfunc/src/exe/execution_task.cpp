56cb43af318fe728ce4412dba1ee3073a5b9ec8d
Shawn Chin <shawnchin@gmail.com>
2012-02-09 18:05:29 +0000

    Missed out execution_task.* files

---
+++ f661-exe_applyfunc/src/exe/execution_task.cpp
@@ -0,0 +1,19 @@
+#include "execution_task.hpp"
+namespace flame { namespace exe { namespace multicore {
+
+// This is a mock implementation. We shouldn't really be passing in a vector by vlue
+ExecutionTask::ExecutionTask(AgentFuncPtr func_ptr, MemVectorPtr mem_ptr)
+{
+    fptr_ = func_ptr;
+    mptr_ = mem_ptr;
+}
+
+MemVectorIteratorPair ExecutionTask::get_mem_iter() {
+    return MemVectorIteratorPair(mptr_->begin(), mptr_->end());
+}
+
+AgentFuncPtr ExecutionTask::get_func_ptr(void) {
+    return fptr_;
+}
+
+}}}
---
+++ f661-exe_applyfunc/src/exe/execution_task.hpp
@@ -0,0 +1,31 @@
+#ifndef EXE__EXECUTION_TASK_HPP
+#define EXE__EXECUTION_TASK_HPP
+
+#include <vector>
+#include "flame.h"
+#include "boost/shared_ptr.hpp"
+
+typedef std::vector<int> MemVector;
+typedef boost::shared_ptr<MemVector> MemVectorPtr;
+typedef std::pair<MemVector::iterator, MemVector::iterator> MemVectorIteratorPair;
+
+
+namespace flame { namespace exe { namespace multicore {
+
+class ExecutionTask
+{
+    public:
+        // Dummy implementation. We really don't want to pass vectors around by value
+        ExecutionTask(AgentFuncPtr func_ptr, MemVectorPtr mem_ptr);
+        MemVectorIteratorPair get_mem_iter(void);
+        AgentFuncPtr get_func_ptr(void);
+    private:
+
+    // TODO: Store task data as separate structure referenced using boost::shared_ptr
+    //        so that we can split tasks into subtasks while sharing the same data
+        AgentFuncPtr fptr_;
+        MemVectorPtr mptr_;
+};
+
+}}}
+#endif // EXE__EXECUTION_TASK_HPP
