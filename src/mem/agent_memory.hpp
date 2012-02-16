4a7eeb5d762ee0523c588ffb188f5faeda494aad
Shawn Chin <shawnchin@gmail.com>
2012-02-16 15:26:54 +0000

    Completed Feature Branch: [#656] Memory Manager: Store data as long vectors, indexed by agent type and mem var name

---
+++ src/mem/agent_memory.hpp
@@ -0,0 +1,74 @@
+/*!
+ * \file src/mem/agent_memory.hpp
+ * \author Shawn Chin
+ * \date 2012
+ * \copyright Copyright (c) 2012 STFC Rutherford Appleton Laboratory
+ * \copyright Copyright (c) 2012 University of Sheffield
+ * \copyright GNU Lesser General Public License
+ * \brief AgentMemory: management and storage class for per-agent memory vectors
+ */
+#ifndef MEM__AGENT_MEMORY_HPP_
+#define MEM__AGENT_MEMORY_HPP_
+#include <map>
+#include <string>
+#include <utility>  // for std::pair
+#include <vector>
+#include <stdexcept>
+#include "boost/any.hpp"
+// #include "boost/unordered_map.hpp"
+// #include "boost/shared_ptr.hpp"
+
+namespace flame { namespace mem {
+
+//! Map container used to store memory vectors
+typedef std::map<std::string, boost::any> MemoryMap;
+// typedef boost::unordered_map<std::string, boost::any> MemoryMap;
+
+//! Key-Value pair for MemoryMap
+typedef std::pair<std::string, boost::any> MemoryMapValue;
+
+class AgentMemory {
+  public:
+    AgentMemory(std::string agent_name, size_t pop_size_hint)
+        : agent_name_(agent_name),
+          population_size_(pop_size_hint) {}
+
+    template <class T>
+    void RegisterVar(std::string var_name) {
+      typedef std::vector<T> vector_T;
+      std::pair<MemoryMap::iterator, bool> ret;
+
+      ret = mem_map_.insert(MemoryMapValue(var_name, vector_T()));
+      if (!ret.second) {  // Ensure that this is an insertion, not replacement
+        throw std::invalid_argument("var with that name already registered");
+      }
+
+      // reserve capacity within inserted vector
+      vector_T &vec = boost::any_cast<vector_T&>(ret.first->second);
+      vec.reserve(population_size_);
+    }
+
+    template <class T>
+    std::vector<T>& GetMemoryVector(std::string const& var_name) {
+      typedef std::vector<T> vector_T;
+
+      const MemoryMap::iterator it = mem_map_.find(var_name);
+      if (it == mem_map_.end()) {  // key not found
+        throw std::invalid_argument("Invalid agent memory variable");
+      }
+
+      try {
+        return boost::any_cast<vector_T&>(it->second);
+      }
+      catch(boost::bad_any_cast E) {
+        throw std::domain_error("Invalid type used");
+      }
+    }
+
+  private:
+    std::string agent_name_;
+    int population_size_;
+    MemoryMap mem_map_;
+};
+}}  // namespace flame::mem
+#endif  // MEM__AGENT_MEMORY_HPP_
---
+++ src/mem/tests/test_agent_memory.cpp
@@ -0,0 +1,50 @@
+/*!
+ * \file src/mem/tests/test_agent_memory.cpp
+ * \author Shawn Chin
+ * \date 2012
+ * \copyright Copyright (c) 2012 STFC Rutherford Appleton Laboratory
+ * \copyright Copyright (c) 2012 University of Sheffield
+ * \copyright GNU Lesser General Public License
+ * \brief Test suite for the memory module
+ */
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include <vector>
+#include "../agent_memory.hpp"
+
+namespace m = flame::mem;
+
+BOOST_AUTO_TEST_SUITE(MemModule)
+
+BOOST_AUTO_TEST_CASE(test_register_var) {
+  size_t size_hint = 1000;
+  m::AgentMemory am("circle", size_hint);
+
+  am.RegisterVar<int>("x_int");
+  am.RegisterVar<double>("y_dbl");
+
+  // retrieve vector that does not exist
+  BOOST_CHECK_THROW(am.GetMemoryVector<int>("z"), std::invalid_argument);
+  // retrieve vector with wrong type
+  BOOST_CHECK_THROW(am.GetMemoryVector<int>("y_dbl"), std::domain_error);
+  // retrieve vector properly
+  BOOST_CHECK_NO_THROW(am.GetMemoryVector<int>("x_int"));
+
+  // Check allocated capacity
+  BOOST_CHECK_EQUAL(am.GetMemoryVector<int>("x_int").capacity(), size_hint);
+  BOOST_CHECK_EQUAL(am.GetMemoryVector<double>("y_dbl").capacity(), size_hint);
+
+  // modify and access vectors directly
+  am.GetMemoryVector<int>("x_int").push_back(10);
+  am.GetMemoryVector<int>("x_int").push_back(20);
+  BOOST_CHECK_EQUAL(am.GetMemoryVector<int>("x_int").size(), (size_t)2);
+
+  // get reference to memory vector
+  std::vector<int> &v = am.GetMemoryVector<int>("x_int");
+  v.push_back(30);
+  v.push_back(40);
+  BOOST_CHECK_EQUAL(v.size(), (unsigned int)4);
+  BOOST_CHECK_EQUAL(am.GetMemoryVector<int>("x_int").size(), (size_t)4);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
