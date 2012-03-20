b65096be37fde310efd2176c1a212570166f56d2
Simon Coakley <sysfal@gmail.com>
2012-03-20 09:49:58 +0000

    Add .hpp to known file extensions

--- f647-ioxml_model/src/tests/cpplint.py
+++ f647-ioxml_model/src/tests/cpplint.py
@@ -3250,7 +3250,7 @@ def ProcessFile(filename, vlevel, extra_check_functions=[]):
   # When reading from stdin, the extension is unknown, so no cpplint tests
   # should rely on the extension.
   if (filename != '-' and file_extension != 'cc' and file_extension != 'h'
-      and file_extension != 'cpp'):
+      and file_extension != 'cpp' and file_extension != 'hpp'):
     sys.stderr.write('Ignoring %s; not a .cc or .h file\n' % filename)
   else:
     ProcessFileData(filename, file_extension, lines, Error,
