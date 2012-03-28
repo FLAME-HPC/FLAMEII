7221ebfffc96e002b712bd41987a09fcc9ac4c8c
Simon Coakley <sysfal@gmail.com>
2012-03-28 12:41:37 +0000

    Add initial files for ioxml_pop. Update Makefile to include libxml. Add data test model.

---
+++ f648-ioxml_pop/src/io/io_xml_pop.cpp
@@ -0,0 +1,23 @@
+/*!
+ * \file src/io/io_xml_pop.cpp
+ * \author Simon Coakley
+ * \date 2012
+ * \copyright Copyright (c) 2012 STFC Rutherford Appleton Laboratory
+ * \copyright Copyright (c) 2012 University of Sheffield
+ * \copyright GNU Lesser General Public License
+ * \brief IOXMLPop: reading of population XML file
+ */
+#include <boost/filesystem.hpp>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include "./io_xml_pop.hpp"
+
+namespace flame { namespace io {
+
+int IOXMLPop::readXMLPop(std::string file_name, XModel * model) {
+    return 0;
+}
+
+}}  // namespace flame::io
---
+++ f648-ioxml_pop/src/io/io_xml_pop.hpp
@@ -0,0 +1,29 @@
+/*!
+ * \file src/io/io_xml_pop.hpp
+ * \author Simon Coakley
+ * \date 2012
+ * \copyright Copyright (c) 2012 STFC Rutherford Appleton Laboratory
+ * \copyright Copyright (c) 2012 University of Sheffield
+ * \copyright GNU Lesser General Public License
+ * \brief IOXMLPop: reading of population XML file
+ */
+#ifndef IO__XML_POP_HPP_
+#define IO__XML_POP_HPP_
+#include <string>
+#include <vector>
+#include "./xmodel.hpp"
+
+namespace flame { namespace io {
+
+class IOXMLPop {
+  public:
+    IOXMLPop() {}
+
+    int readXMLPop(std::string file_name, XModel * model);
+
+  private:
+
+};
+}}  // namespace flame::io
+#endif  // IO__XML_POP_HPP_
+
