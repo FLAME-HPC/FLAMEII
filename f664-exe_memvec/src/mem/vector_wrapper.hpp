340c15fedbdb9288246b98b6d75cd65bf58847f6
Shawn Chin <shawnchin@gmail.com>
2012-05-11 11:01:45 +0000

    Start Feature Branch: (EXE) request list of memory vectors from Memory manager

---
+++ f664-exe_memvec/src/mem/vector_wrapper.hpp
@@ -0,0 +1,58 @@
+/*!
+ * \file FILENAME
+ * \author Shawn Chin
+ * \date 2012
+ * \copyright Copyright (c) 2012 STFC Rutherford Appleton Laboratory
+ * \copyright Copyright (c) 2012 University of Sheffield
+ * \copyright GNU Lesser General Public License
+ * \brief DESCRIPTION
+ */
+#ifndef MEM__MEMORY_VECTOR_HPP
+#define MEM__MEMORY_VECTOR_HPP
+#include <vector>
+
+namespace flame { namespace mem {
+
+//! Base for vector classes that can be used as proxy object for
+//! agent memory vectors.
+class VectorWrapperBase {
+  public:
+    virtual ~VectorWrapperBase() {}
+    virtual void reserve(unsigned int n) = 0;
+    virtual void* GetVectorPtr() = 0;
+    virtual const std::type_info* GetDataType() const = 0;
+
+    //! Simulate a virtual copy constructor
+    virtual VectorWrapperBase* clone() const = 0;
+};
+
+//! Make VectorWrapperBase cloneable within boost::ptr_map
+inline VectorWrapperBase* new_clone(const VectorWrapperBase& a) {
+  return a.clone();
+}
+
+template <typename T>
+class VectorWrapper: public VectorWrapperBase {
+  public:
+    VectorWrapper() { data_type_ = &typeid(T); }
+    typedef T data_type;
+    typedef std::vector<T> vector_type;
+    void reserve(unsigned int n) { v_.reserve(n); }
+
+    void* GetVectorPtr() {
+      return &v_;
+    }
+
+    virtual const std::type_info* GetDataType() const {
+      return data_type_;
+    }
+
+    VectorWrapper<T>* clone() const { return new VectorWrapper<T>(*this); }
+  private:
+    const std::type_info *data_type_;
+    std::vector<T> v_;
+};
+
+
+}}  // namespace flame::mem
+#endif  // MEM__MEMORY_VECTOR_HPP
---
+++ f664-exe_memvec/src/model/model_manager.cpp
@@ -0,0 +1,56 @@
+/*!
+ * \file src/model/model_manager.cpp
+ * \author Simon Coakley
+ * \date 2012
+ * \copyright Copyright (c) 2012 STFC Rutherford Appleton Laboratory
+ * \copyright Copyright (c) 2012 University of Sheffield
+ * \copyright GNU Lesser General Public License
+ * \brief ModelManager: management and storage class for model data
+ */
+#include <string>
+#include <vector>
+#include "./model_manager.hpp"
+#include "../io/io_manager.hpp"
+
+namespace flame { namespace model {
+
+ModelManager::~ModelManager() {
+    /* Free task list memory */
+    Task * task;
+    while (!tasks_.empty()) {
+        task = tasks_.back();
+        delete task;
+        tasks_.pop_back();
+    }
+}
+
+int ModelManager::loadModel(std::string const& file) {
+    int rc;
+    flame::io::IOManager ioManager;
+
+    /* Read model */
+    rc = ioManager.loadModel(file, &model_);
+    if (rc != 0) {
+        std::fprintf(stderr,
+            "Error: Model XML file could not be read.\n");
+        model_.clear();
+        return 1;
+    }
+
+    /* Validate model */
+    rc = model_.validate();
+    if (rc != 0) {
+        std::fprintf(stderr,
+            "Error: Model XML file could not be validated.\n");
+        model_.clear();
+        return 2;
+    }
+
+    return 0;
+}
+
+std::vector<Task*> * ModelManager::get_task_list() {
+    return &tasks_;
+}
+
+}}  // namespace flame::model
