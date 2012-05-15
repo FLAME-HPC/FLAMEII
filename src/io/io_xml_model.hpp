2bc886f5dd2ddfe04db7dcdc64a8abbef99fdd3e
Simon Coakley <sysfal@gmail.com>
2012-05-15 09:13:06 +0000

    Completed Feature Branch: [#732] (rewrite of f647) I/O Backend for XML (model)

--- src/io/io_xml_model.hpp
+++ src/io/io_xml_model.hpp
@@ -9,7 +9,8 @@
  */
 #ifndef IO__XML_MODEL_HPP_
 #define IO__XML_MODEL_HPP_
-#include <boost/property_tree/ptree.hpp>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
 #include <string>
 #include <vector>
 #include "../model/model_manager.hpp"
@@ -25,51 +26,33 @@ class IOXMLModel {
     int readXMLModel(std::string file_name, model::XModel * model);
 
   private:
-    int readUnknownElement(boost::property_tree::ptree::value_type const& v);
-    int readIncludedModels(boost::property_tree::ptree::value_type const& v,
+    int readUnknownElement(xmlNode * node);
+    int readIncludedModels(xmlNode * node,
             std::string directory, model::XModel * model);
-    int readIncludedModel(boost::property_tree::ptree::value_type const& v,
+    int readIncludedModel(xmlNode * node,
             std::string directory, model::XModel * model);
-    int readEnvironment(boost::property_tree::ptree::value_type const& v,
-            model::XModel * model);
-    int readFunctionFiles(boost::property_tree::ptree::value_type const& v,
-            model::XModel * model);
-    int readDataTypes(boost::property_tree::ptree::value_type const& v,
-            model::XModel * model);
-    int readDataType(boost::property_tree::ptree::value_type const& v,
-            model::XModel * model);
-    int readTimeUnits(boost::property_tree::ptree::value_type const& v,
-            model::XModel * model);
-    int readTimeUnit(boost::property_tree::ptree::value_type const& v,
-            model::XModel * model);
-    int readVariables(boost::property_tree::ptree::value_type const& v,
+    int readEnvironment(xmlNode * node, model::XModel * model);
+    int readFunctionFiles(xmlNode * node, model::XModel * model);
+    int readDataTypes(xmlNode * node, model::XModel * model);
+    int readDataType(xmlNode * node, model::XModel * model);
+    int readTimeUnits(xmlNode * node, model::XModel * model);
+    int readTimeUnit(xmlNode * node, model::XModel * model);
+    int readVariables(xmlNode * node,
             std::vector<model::XVariable*> * variables);
-    int readVariable(boost::property_tree::ptree::value_type const& v,
+    int readVariable(xmlNode * node,
             std::vector<model::XVariable*> * variables);
-    int readAgents(boost::property_tree::ptree::value_type const& v,
-            model::XModel * model);
-    int readAgent(boost::property_tree::ptree::value_type const& v,
-            model::XModel * model);
-    int readTransitions(boost::property_tree::ptree::value_type const& v,
-            model::XMachine * machine);
-    int readTransition(boost::property_tree::ptree::value_type const& v,
-            model::XMachine * machine);
-    int readInputs(boost::property_tree::ptree::value_type const& v,
-            model::XFunction * xfunction);
-    int readInput(boost::property_tree::ptree::value_type const& v,
-            model::XFunction * xfunction);
-    int readOutputs(boost::property_tree::ptree::value_type const& v,
-            model::XFunction * xfunction);
-    int readOutput(boost::property_tree::ptree::value_type const& v,
-            model::XFunction * xfunction);
-    int readMessages(boost::property_tree::ptree::value_type const& v,
-            model::XModel * model);
-    int readMessage(boost::property_tree::ptree::value_type const& v,
-            model::XModel * model);
-    int readSort(boost::property_tree::ptree::value_type const& v,
-            model::XIOput * xioput);
-    int readCondition(boost::property_tree::ptree::value_type const& v,
-            model::XCondition * xcondition);
+    int readAgents(xmlNode * node, model::XModel * model);
+    int readAgent(xmlNode * node, model::XModel * model);
+    int readTransitions(xmlNode * node, model::XMachine * machine);
+    int readTransition(xmlNode * node, model::XMachine * machine);
+    int readInputs(xmlNode * node, model::XFunction * xfunction);
+    int readInput(xmlNode * node, model::XFunction * xfunction);
+    int readOutputs(xmlNode * node, model::XFunction * xfunction);
+    int readOutput(xmlNode * node, model::XFunction * xfunction);
+    int readMessages(xmlNode * node, model::XModel * model);
+    int readMessage(xmlNode * node, model::XModel * model);
+    int readSort(xmlNode * node, model::XIOput * xioput);
+    int readCondition(xmlNode * node, model::XCondition * xcondition);
 };
 }}}  // namespace flame::io::xml
 #endif  // IO__XML_MODEL_HPP_
