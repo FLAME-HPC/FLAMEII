264e4f6de80ebb33b5d87ea51b92ce83b8b0d759
Shawn Chin <shawnchin@gmail.com>
2012-06-22 12:50:07 +0000

    Start Feature Branch: Thread-local message boards (multicore)

---
+++ f670-mb_multicore/src/exe/task_queue_interface.hpp
@@ -0,0 +1,49 @@
+/*!
+ * \file src/exe/task_queue_interface.hpp
+ * \author Shawn Chin
+ * \date 2012
+ * \copyright Copyright (c) 2012 STFC Rutherford Appleton Laboratory
+ * \copyright Copyright (c) 2012 University of Sheffield
+ * \copyright GNU Lesser General Public License
+ * \brief Interface class for Task Queues
+ */
+#ifndef EXE__TASK_QUEUE_INTERFACE_HPP_
+#define EXE__TASK_QUEUE_INTERFACE_HPP_
+#include "boost/thread/mutex.hpp"
+#include "boost/thread/condition_variable.hpp"
+#include "task_interface.hpp"
+
+namespace flame { namespace exe {
+
+typedef boost::function<void (Task::id_type)> TaskQueueCallback;
+
+class TaskQueue {
+  friend class WorkerThread;
+  public:
+    virtual ~TaskQueue() {}
+
+    //! Adds a task to the queue
+    virtual void Enqueue(Task::id_type task_id) = 0;
+
+    //! Indicates that a task has been completed
+    virtual void TaskDone(Task::id_type task_id) = 0;
+
+    //! Waits for and returns the next available task
+    virtual Task::id_type GetNextTask() = 0;
+
+    //! Returns true if the queue is empty
+    virtual bool empty() const = 0;
+
+    //! Sets the callback function to be called when a task is completed
+    void SetCallback(TaskQueueCallback func) {
+      callback_ = func;
+    }
+
+  protected:
+    boost::mutex mutex_;
+    boost::condition_variable ready_;
+    TaskQueueCallback callback_;
+};
+
+}}  // namespace flame::exe
+#endif  // EXE__TASK_QUEUE_INTERFACE_HPP_
---
+++ f670-mb_multicore/src/model/xtimeunit.cpp
@@ -0,0 +1,57 @@
+/*!
+ * \file src/model/xtimeunit.cpp
+ * \author Simon Coakley
+ * \date 2012
+ * \copyright Copyright (c) 2012 STFC Rutherford Appleton Laboratory
+ * \copyright Copyright (c) 2012 University of Sheffield
+ * \copyright GNU Lesser General Public License
+ * \brief XTimeUnit: holds time unit information
+ */
+#include <cstdio>
+#include <string>
+#include "./xtimeunit.hpp"
+
+namespace flame { namespace model {
+
+XTimeUnit::~XTimeUnit() {
+}
+
+void XTimeUnit::print() {
+    std::fprintf(stdout, "\tTime Unit Name: %s\n", getName().c_str());
+    std::fprintf(stdout, "\tUnit  : %s\n", getUnit().c_str());
+    std::fprintf(stdout, "\tPeriod: %d\n", getPeriod());
+}
+
+void XTimeUnit::setName(std::string name) {
+    name_ = name;
+}
+
+std::string XTimeUnit::getName() {
+    return name_;
+}
+
+void XTimeUnit::setUnit(std::string unit) {
+    unit_ = unit;
+}
+
+std::string XTimeUnit::getUnit() {
+    return unit_;
+}
+
+void XTimeUnit::setPeriodString(std::string period) {
+    periodString_ = period;
+}
+
+std::string XTimeUnit::getPeriodString() {
+    return periodString_;
+}
+
+void XTimeUnit::setPeriod(int period) {
+    period_ = period;
+}
+
+int XTimeUnit::getPeriod() {
+    return period_;
+}
+
+}}  // namespace flame::model
