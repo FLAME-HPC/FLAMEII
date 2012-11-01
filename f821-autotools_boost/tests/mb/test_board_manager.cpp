caa99af47e7b50df350f513b53bb3d1f891d6077
Shawn Chin <shawnchin@gmail.com>
2012-11-01 10:49:08 +0000

    fixed includes

--- f821-autotools_boost/tests/mb/test_board_manager.cpp
+++ f821-autotools_boost/tests/mb/test_board_manager.cpp
@@ -9,9 +9,9 @@
  */
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
-#include "mb/message_board_manager.hpp"
-#include "mb/board_writer.hpp"
-#include "mb/message_iterator.hpp"
+#include "flame2/mb/message_board_manager.hpp"
+#include "flame2/mb/board_writer.hpp"
+#include "flame2/mb/message_iterator.hpp"
 
 BOOST_AUTO_TEST_SUITE(MBModule)
 
