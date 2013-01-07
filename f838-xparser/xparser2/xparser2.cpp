bff226eecd846b2333718a67c0ffa2357cd6f121
Shawn Chin <shawnchin@gmail.com>
2013-01-07 15:32:20 +0000

    Initialise files for xparser2

---
+++ f838-xparser/xparser2/xparser2.cpp
@@ -0,0 +1,17 @@
+/*!
+ * \file xparser2/xparser2.cpp
+ * \author Shawn Chin, Simon Coakley
+ * \date January 2013
+ * \copyright Copyright (c) 2013 STFC Rutherford Appleton Laboratory
+ * \copyright Copyright (c) 2013 University of Sheffield
+ * \copyright GNU Lesser General Public License
+ * \brief Xparser
+ */
+#include <iostream>
+#include "flame2/model/xmodel.hpp"
+#include "xparser2.hpp"
+
+int main(int argc, const char* argv[]) {
+  flame::model::XModel model;  // just to check if we can access the module
+  std::cout << "Parse me" << std::endl;
+}
---
+++ f838-xparser/xparser2/xparser2.hpp
@@ -0,0 +1,9 @@
+/*!
+ * \file xparser2/xparser2.hpp
+ * \author Shawn Chin, Simon Coakley
+ * \date January 2013
+ * \copyright Copyright (c) 2013 STFC Rutherford Appleton Laboratory
+ * \copyright Copyright (c) 2013 University of Sheffield
+ * \copyright GNU Lesser General Public License
+ * \brief Xparser
+ */
