f13e08e5b0f14dbf4fc9aedf924b94e15cd3d030
Shawn Chin <shawnchin@gmail.com>
2013-01-07 17:20:45 +0000

    Printer class to handle text output. Handles indentation and variable substitution.

---
+++ f838-xparser/xparser2/printer.cpp
@@ -0,0 +1,136 @@
+/*!
+ * \file xparser2/printer.cpp
+ * \author Shawn Chin
+ * \date January 2013
+ * \copyright Copyright (c) 2013 STFC Rutherford Appleton Laboratory
+ * \copyright Copyright (c) 2013 University of Sheffield
+ * \copyright GNU Lesser General Public License
+ * \brief Utility class for writing text to an output stream
+ */
+#include <map>
+#include <string>
+#include <cstring>
+#include "flame2/exceptions/base.hpp"
+#include "printer.hpp"
+
+namespace xparser {
+
+Printer::Printer(std::ostream &s, char delimiter)
+  : s_(s), delim_(delimiter), at_start_of_line_(true) {
+}
+
+void Printer::Indent() {
+  indent_ += "  ";
+}
+
+void Printer::Outdent() {
+  if (indent_.empty()) {
+    throw flame::exceptions::invalid_operation(
+      "Outdent() without matching Indent().");
+  }
+  indent_.resize(indent_.size() - 2);
+}
+
+void Printer::write_(const char* data, int size) {
+  if (at_start_of_line_ && (size > 0) && (data[0] != '\n')) {
+    at_start_of_line_ = false;
+    s_.write(indent_.data(), indent_.size());  // place indent string
+  }
+  s_.write(data, size);
+}
+
+void Printer::Print(const char* text,
+                    const std::map<std::string, std::string>& vars) {
+  int size = strlen(text);
+  int pos = 0;
+
+  // Iterate through string so we can indent lines and perform var substitution
+  for (int i = 0; i < size; ++i) {
+    if (text[i] == '\n') {  // found new line
+      // Write what we have so far, including the newline
+      write_(text + pos, i - pos + 1);
+      pos = i + i;
+      
+      // next write_() should insert an indent
+      at_start_of_line_ = true;
+
+    } else if (text[i] == delim_) {  // found substitution candidate
+      // First, dump previously seen text
+      write_(text + pos, i - pos);
+      pos = i + 1;
+
+      // Locate closing delimiter
+      const char* end = strchr(text + pos, delim_);
+      if (end == NULL) {
+        throw flame::exceptions::invalid_argument(
+          "Closing delimiter not found");
+      }
+      
+      int endpos = end - text;
+      std::string varname(text + pos, endpos - pos);
+      if (varname.empty()) {
+        // Two delimiters in a row reduce to a literal delimiter character.
+        write_(&delim_, 1);
+      } else {
+        // lookup var map and perform substitution
+        std::map<std::string, std::string>::const_iterator \
+            iter = vars.find(varname);
+        if (iter == vars.end()) {
+          throw flame::exceptions::invalid_argument(
+              std::string("Unknown var - ") + varname);
+        } else {
+          write_(iter->second.data(), iter->second.size());
+        }
+      }
+
+      // advance past the var placeholder
+      i = endpos;
+      pos = endpos + 1;
+    }
+  }
+
+  // Write out the remainder of the string
+  //write_(text + pos, size - pos);
+}
+
+void Printer::Print(const char* text) {
+  static std::map<std::string, std::string> empty;
+  Print(text, empty);
+}
+
+void Printer::Print(const char* text,
+                    const char* var, const std::string& value) {
+  std::map<std::string, std::string> vars;
+  vars[var] = value;
+  Print(text, vars);
+}
+
+void Printer::Print(const char* text,
+                    const char* var1, const std::string& value1,
+                    const char* var2, const std::string& value2) {
+  std::map<std::string, std::string> vars;
+  vars[var1] = value1;
+  vars[var2] = value2;
+  Print(text, vars);
+}
+
+void Printer::Print(const char* text,
+                    const char* var1, const std::string& value1,
+                    const char* var2, const std::string& value2,
+                    const char* var3, const std::string& value3) {
+  std::map<std::string, std::string> vars;
+  vars[var1] = value1;
+  vars[var2] = value2;
+  vars[var3] = value3;
+  Print(text, vars);
+}
+
+void Printer::PrintRaw(const std::string& text) {
+  write_(text.data(), text.size());
+}
+
+void Printer::PrintRaw(const char* text) {
+  write_(text, strlen(text));
+}
+
+}  // namespace xparser
---
+++ f838-xparser/xparser2/printer.hpp
@@ -0,0 +1,62 @@
+/*!
+ * \file xparser2/printer.hpp
+ * \author Shawn Chin
+ * \date January 2013
+ * \copyright Copyright (c) 2013 STFC Rutherford Appleton Laboratory
+ * \copyright Copyright (c) 2013 University of Sheffield
+ * \copyright GNU Lesser General Public License
+ * \brief Utility class for writing text to an output stream
+ */
+#ifndef XPARSER__PRINTER_HPP_
+#define XPARSER__PRINTER_HPP_
+#include <ostream>
+#include <map>
+#include <string>
+
+namespace xparser {
+
+class Printer {
+  public:
+    Printer(std::ostream &s, char delimiter = '$');
+    
+    void Print(const char* text);
+    void Print(const char* text,
+               const std::map<std::string, std::string>& vars);
+
+    // Aliases for var mappings with a only a few vars
+    void Print(const char* text,
+               const char* var, const std::string& value);
+    void Print(const char* text,
+               const char* var1, const std::string& value1,
+               const char* var2, const std::string& value2);
+    void Print(const char* text,
+               const char* var1, const std::string& value1,
+               const char* var2, const std::string& value2,
+               const char* var3, const std::string& value3);
+
+    // Print string as is. No indentation. No var replacement.
+    void PrintRaw(const std::string& text);
+    void PrintRaw(const char* text);
+    
+    //void PrintFile(const char* filename);
+    //void PrintFile(const char* filename,
+    //               const std::map<std::string, std::string>& vars);
+    //void PrintFileRaw(const char* filename);
+
+    void Indent();
+    void Outdent();
+    
+  private:
+    std::ostream &s_;
+    std::string indent_;
+    char delim_;
+    bool at_start_of_line_;
+  
+    void write_(const char* data, int size);
+    
+    Printer(const Printer&);
+    void operator=(const Printer&);
+};
+
+}  // namespace xparser
+#endif  // XPARSER__PRINTER_HPP_
