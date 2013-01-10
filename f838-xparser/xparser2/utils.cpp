dfb0c8dcadb87c560bd3b02569b7bb1a97bd5961
Shawn Chin <shawnchin@gmail.com>
2013-01-10 11:16:41 +0000

    Added debug flags and temporary couts for locate_template

--- f838-xparser/xparser2/utils.cpp
+++ f838-xparser/xparser2/utils.cpp
@@ -13,11 +13,11 @@
 namespace xparser { namespace utils {
 
 std::string locate_template(const char* template_name) {
-  const char* tmpl_dir = "xparser2/templates";  // template subdir
 
   // first, look for the file relative to the current working directory
   boost::filesystem::path cwd(boost::filesystem::current_path());
-  boost::filesystem::path out = cwd / tmpl_dir / template_name;
+  boost::filesystem::path out = cwd / "xparser2/templates" / template_name;
+  std::cout << "... [locate_template] looking in " << out << std::endl;
   if (boost::filesystem::exists(out) &&
       boost::filesystem::is_regular_file(out)) {
     return out.string();
@@ -26,7 +26,8 @@ std::string locate_template(const char* template_name) {
   // if package data install dir defined, look there next
   #ifdef PKGDATADIR
     boost::filesystem::path pkgdir(PKGDATADIR);
-    out = pkgdir / tmpl_dir / template_name;
+    out = pkgdir / template_name;
+    std::cout << "... [locate_template] looking in " << out << std::endl;
     if (boost::filesystem::exists(out) &&
       boost::filesystem::is_regular_file(out)) {
     return out.string();
