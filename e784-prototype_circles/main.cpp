e9e93f24da988579cebfe7636c12c5b432b70c54
Simon Coakley <sysfal@gmail.com>
2013-01-11 14:36:18 +0000

    Working version

--- e784-prototype_circles/main.cpp
+++ e784-prototype_circles/main.cpp
@@ -1,16 +1,8 @@
 #include <cstdio>
 #include <iostream>
+#include <string>
 #include <sys/time.h>
-#include "flame2/exe/task_manager.hpp"
-#include "flame2/exe/scheduler.hpp"
-#include "flame2/exe/splitting_fifo_task_queue.hpp"
-#include "flame2/io/io_manager.hpp"
-#include "flame2/model/model.hpp"
-#include "flame2/mb/message_board_manager.hpp"
-#include "flame2/sim/simulation.hpp"
 
-#include "agent_functions.hpp"
-#include "message_datatypes.hpp"
 
 // returns time in seconds
 static double get_time(void) {
@@ -42,58 +34,37 @@ int main(int argc, const char* argv[]) {
       exit(3);
     }
   }
-  
-  // Create model
+
   model::Model model;
-  model::XModel * xmodel = model.getXModel();
-  model::XMachine * circle_agent = xmodel->addAgent("Circle");
-  circle_agent->addVariable("int", "id");
-  circle_agent->addVariable("double", "x");
-  circle_agent->addVariable("double", "y");
-  circle_agent->addVariable("double", "fx");
-  circle_agent->addVariable("double", "fy");
-  circle_agent->addVariable("double", "radius");
 
-  model::XFunction * function0 = circle_agent->addFunction("outputdata", "0", "1");
-  function0->setMemoryAccessInfoAvailable(true);
-  function0->addReadOnlyVariable("x");
-  function0->addReadOnlyVariable("y");
-  function0->addReadOnlyVariable("radius");
-  function0->addReadOnlyVariable("id");
-  function0->addOutput("location");
-
-  model::XFunction * function1 = circle_agent->addFunction("inputdata", "1", "2");
-  function1->setMemoryAccessInfoAvailable(true);
-  function1->addReadOnlyVariable("x");
-  function1->addReadOnlyVariable("y");
-  function1->addReadOnlyVariable("radius");
-  function1->addReadOnlyVariable("id");
-  function1->addReadWriteVariable("fx");
-  function1->addReadWriteVariable("fy");
-  function1->addInput("location");
-
-  model::XFunction * function2 = circle_agent->addFunction("move", "2", "3");
-  function2->setMemoryAccessInfoAvailable(true);
-  function2->addReadWriteVariable("x");
-  function2->addReadWriteVariable("y");
-  function2->addReadOnlyVariable("fx");
-  function2->addReadOnlyVariable("fy");
-
-  model::XMessage * xmessage = xmodel->addMessage("location");
-  xmessage->addVariable("double", "x");
-  xmessage->addVariable("double", "y");
-  xmessage->addVariable("double", "z");
-  xmessage->addVariable("int", "id");
-
-  // Validate model
-  xmodel->validate();
-
-  // Register agent functions
-  model.registerAgentFunction("outputdata", &outputdata);
-  model.registerAgentFunction("inputdata", &inputdata);
-  model.registerAgentFunction("move", &move);
-  // Register message types
-  model.registerMessageType<location_message>("location");
+  model.addAgent("Circle");
+  model.addAgentVariable("Circle", "int", "id");
+  model.addAgentVariable("Circle", "double", "x");
+  model.addAgentVariable("Circle", "double", "y");
+  model.addAgentVariable("Circle", "double", "fx");
+  model.addAgentVariable("Circle", "double", "fy");
+  model.addAgentVariable("Circle", "double", "radius");
+  model.addAgentFunction("Circle", "outputdata", "start", "1");
+  model.addAgentFunctionReadOnlyVariable("Circle", "outputdata", "start", "1", "x");
+  model.addAgentFunctionReadOnlyVariable("Circle", "outputdata", "start", "1", "y");
+  model.addAgentFunctionReadOnlyVariable("Circle", "outputdata", "start", "1", "radius");
+  model.addAgentFunctionReadOnlyVariable("Circle", "outputdata", "start", "1", "id");
+  model.addAgentFunction("Circle", "inputdata", "1", "2");
+  model.addAgentFunctionReadOnlyVariable("Circle", "inputdata", "1", "2", "x");
+  model.addAgentFunctionReadOnlyVariable("Circle", "inputdata", "1", "2", "y");
+  model.addAgentFunctionReadOnlyVariable("Circle", "inputdata", "1", "2", "radius");
+  model.addAgentFunctionReadOnlyVariable("Circle", "inputdata", "1", "2", "id");
+  model.addAgentFunctionReadWriteVariable("Circle", "inputdata", "1", "2", "fx");
+  model.addAgentFunctionReadWriteVariable("Circle", "inputdata", "1", "2", "fy");
+  model.addAgentFunction("Circle", "move", "2", "end");
+  model.addAgentFunctionReadOnlyVariable("Circle", "move", "2", "end", "fx");
+  model.addAgentFunctionReadOnlyVariable("Circle", "move", "2", "end", "fy");
+  model.addAgentFunctionReadWriteVariable("Circle", "move", "2", "end", "x");
+  model.addAgentFunctionReadWriteVariable("Circle", "move", "2", "end", "y");
+  model.addMessage("location");
+  model.addMessageVariable("location", "int", "id");
+  model.addMessageVariable("location", "double", "x");
+  model.addMessageVariable("location", "double", "y");
 
   // Create simulation using model and path to initial pop
   flame::sim::Simulation s(&model, pop_path);
