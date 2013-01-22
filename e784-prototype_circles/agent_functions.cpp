c93f118248f0a64ba8d2d8a5ef53bf1d19435cff
Simon Coakley <sysfal@gmail.com>
2013-01-22 14:19:02 +0000

    Fix some lint warnings

--- e784-prototype_circles/agent_functions.cpp
+++ e784-prototype_circles/agent_functions.cpp
@@ -10,7 +10,7 @@ FLAME_AGENT_FUNCTION(outputdata) {
   msg.x = FLAME.GetMem<double>("x");
   msg.y = FLAME.GetMem<double>("y");
   msg.id = FLAME.GetMem<int>("id");
-  
+
   /* post message with current location */
   FLAME.PostMessage("location", msg);
   return FLAME_AGENT_ALIVE;
@@ -22,18 +22,18 @@ FLAME_AGENT_FUNCTION(inputdata) {
   double x = FLAME.GetMem<double>("x");
   double y = FLAME.GetMem<double>("y");
   double diameter = FLAME.GetMem<double>("radius") * 2;
-  
+
   /* tmp vars */
   double p, core_distance;
   double fx = 0, fy = 0;
-  
+
   /* loop through messages */
   MessageIterator iter = FLAME.GetMessageIterator("location");
   for (; !iter.AtEnd(); iter.Next()) {
     msg = iter.GetMessage<location_message>();
     if (msg.id != id) {
       core_distance = distance(x, y, msg.x, msg.y);
-      
+
       if (core_distance < diameter) {
         p = kr * diameter / core_distance;
         /* accumulate forces */
@@ -42,17 +42,19 @@ FLAME_AGENT_FUNCTION(inputdata) {
       }
     }
   }
-  
+
   /* store forces in agent mem */
   FLAME.SetMem<double>("fx", fx);
   FLAME.SetMem<double>("fy", fy);
-  
+
   return FLAME_AGENT_ALIVE;
 }
 
 FLAME_AGENT_FUNCTION(move) {
   /* update position based on accumulated forces */
-  FLAME.SetMem<double>("x", FLAME.GetMem<double>("x") + FLAME.GetMem<double>("fx"));
-  FLAME.SetMem<double>("y", FLAME.GetMem<double>("y") + FLAME.GetMem<double>("fy"));
+  FLAME.SetMem<double>("x",
+      FLAME.GetMem<double>("x") + FLAME.GetMem<double>("fx"));
+  FLAME.SetMem<double>("y",
+      FLAME.GetMem<double>("y") + FLAME.GetMem<double>("fy"));
   return FLAME_AGENT_ALIVE;
 }
