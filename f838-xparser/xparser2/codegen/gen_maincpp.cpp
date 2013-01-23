781262edd4634d666bdd965bf3fbf0f7906e183c
Shawn Chin <shawnchin@gmail.com>
2013-01-23 15:26:37 +0000

    properly handle invalid input args

--- f838-xparser/xparser2/codegen/gen_maincpp.cpp
+++ f838-xparser/xparser2/codegen/gen_maincpp.cpp
@@ -22,6 +22,8 @@ GenMainCpp::GenMainCpp() {
   RequireSysHeader("iostream");
   // flame headers
   RequireHeader("flame2/sim/simulation.hpp");  // used in main_footer.cpp.tmpl
+  RequireHeader("flame2/exceptions/io.hpp");
+  RequireHeader("flame2/exceptions/exe.hpp");
 }
 
 void GenMainCpp::Generate(Printer* printer) const {
